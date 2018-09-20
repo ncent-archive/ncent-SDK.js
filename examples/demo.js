b5afdecb9b5be6d585488db85ae2a20f650ccac1
Dustin Pearson <dustinparkerpearson@gmail.com>
2018-09-19 20:28:10 -0700

    Add end-to-end testing / demo

--- examples/demo.js
+++ examples/demo.js
@@ -1,66 +1,106 @@
 // const nCentSDK = require('ncent-sandbox-sdk');
-const nCentSDK = require('../source/ncentSDK');
+const nCentSDK = require("../source/ncentSDK");
 
 const sdk = new nCentSDK("http://localhost:8010/api");
 
-const TOKEN_NAME = 'kkkkkCent'; // MUST BE UNIQUE
+const TOKEN_NAME = "tttt"; // MUST BE UNIQUE
 const TOKEN_COUNT = 10000;
-const EXPIRATION_YEAR = '2021';
-const TRANSFER_AMOUNT = 100;
+const EXPIRATION_YEAR = "2021";
+
+const transferTokens = async (sponsorKeypair, tokenTypeUuid, receiverKeypair, amount) => {
+  const createdChallengeRet = await sdk.createChallenge(
+    sponsorKeypair,
+    tokenTypeUuid,
+    amount
+  );
+  const createdChallenge = createdChallengeRet.data;
+  console.log(
+    `${sponsorKeypair.publicKey()} created challenge ${createdChallenge.uuid} with ${amount} of ${tokenTypeUuid}\n`
+  );
+  const sharedChallengeRet = await sdk.shareChallenge(
+    sponsorKeypair,
+    createdChallenge.uuid,
+    receiverKeypair.publicKey()
+  );
+  const sharedChallenge = sharedChallengeRet.data.transaction;
+  console.log(`${sponsorKeypair.publicKey()} shared the challenge with ${receiverKeypair.publicKey()}\n`);
+  return sharedChallenge;
+};
+
+const redeemChallenge = async (sponsorKeypair, transactionUuid) => {
+  const redeemRet = await sdk.redeemChallenge(sponsorKeypair, transactionUuid);
+  const redemptionTransaction = redeemRet.data.transaction;
+  console.log(`Redeemed transaction ${redemptionTransaction.uuid}\n`);
+  return redemptionTransaction;
+};
+
+const retrieveProvenanceChainFIFO = async (tokenTypeUuid, publicKey) => {
+  const retProv = await sdk.retrieveProvenanceChainFIFO(tokenTypeUuid, publicKey);
+  const provenanceChain = retProv.data;
+  console.log(`Retrieved provenanceChain of oldest owned challenge of ${publicKey} for tokenType ${tokenTypeUuid}\n`);
+  console.log(provenanceChain, '\n');
+  return provenanceChain;
+}
+
+const shareChallenge = async (senderKeypair, receiverPublicKey, transactionUuid) => {
+  const sharedChallengeRet = await sdk.shareChallenge(
+    senderKeypair,
+    transactionUuid,
+    receiverPublicKey
+  );
+  const sharedChallenge = sharedChallengeRet.data.transaction;
+  console.log(
+    `${senderKeypair.publicKey()} shared the ${transactionUuid} challenge with ${receiverPublicKey}`
+  );
+  return shareChallenge;
+};
 
 const demo = async () => {
   try {
-    const keypair1 = sdk.createWalletAddress();
-    const secondGiver = sdk.createWalletAddress();
+    const tokenTypeSponsor = sdk.createWalletAddress();
     const keypair2 = sdk.createWalletAddress();
     const keypair3 = sdk.createWalletAddress();
     const stampRet = await sdk.stampToken(
-      keypair1.publicKey(),
+      tokenTypeSponsor.publicKey(),
       TOKEN_NAME,
       TOKEN_COUNT,
-      EXPIRATION_YEAR,
+      EXPIRATION_YEAR
     );
-    const tokenId = stampRet.data['token']['uuid'];
-    const transferRet = await sdk.transferTokens(
-      keypair1,
-      keypair2.publicKey(),
-      tokenId,
-      TRANSFER_AMOUNT,
+    const stampedTokenType = stampRet.data.tokenType;
+    console.log(
+      `${tokenTypeSponsor.publicKey()} created tokenType with UUID ${stampedTokenType.uuid}\n`,
     );
-    const txn = transferRet.data['txn'];
-    console.log(`Transferred ${txn.amount} of ${tokenId} from ${txn.fromAddress} to ${txn.toAddress}`);
-    const transferRet2 = await sdk.transferTokens(
-      keypair1,
-      secondGiver.publicKey(),
-      tokenId,
-      TRANSFER_AMOUNT,
-      );
-      const txn2 = transferRet2.data['txn'];
-      console.log(`Transferred ${txn2.amount} of ${tokenId} from ${txn2.fromAddress} to ${txn2.toAddress}`);
-    const transferRet3 = await sdk.transferTokens(
+    const sharedChallenge1 = await transferTokens(
+      tokenTypeSponsor,
+      stampedTokenType.uuid,
+      keypair2,
+      100
+    );
+    const sharedChallenge2 = await transferTokens(
+      tokenTypeSponsor,
+      stampedTokenType.uuid,
+      keypair3,
+      100
+    );
+    const sharedChallenge3 = await shareChallenge(
       keypair2,
       keypair3.publicKey(),
-      tokenId,
-      TRANSFER_AMOUNT,
+      sharedChallenge1.uuid
     );
-    const txn3 = transferRet3.data["txn"];
-    console.log(`Transferred ${txn3.amount} of ${tokenId} from ${txn3.fromAddress} to ${txn3.toAddress}`);
-    const transferRet4 = await sdk.transferTokens(
-      secondGiver,
-      keypair3.publicKey(),
-      tokenId,
-      TRANSFER_AMOUNT,
+    const provenanceChain1 = await retrieveProvenanceChainFIFO(
+      stampedTokenType.uuid,
+      keypair3.publicKey()
     );
-    const txn4 = transferRet4.data["txn"];
-    console.log(`Transferred ${txn4.amount} of ${tokenId} from ${txn4.fromAddress} to ${txn4.toAddress}`);
-    const currentKeyPair3Wallet = await sdk.getTokenBalance(keypair3.publicKey(), tokenId);
-    console.log(`Wallet with public key ${keypair3.publicKey()} has ${currentKeyPair3Wallet.data[0].balance} tokens`);
-    const chain = await sdk.getProvenanceChain(tokenId, keypair3.publicKey());
-    console.log(`Here is the provenance chain of that wallet:`, chain.data);
-  } catch (error) {
-    error.response.data.errors.forEach((error)=>{
-      console.log(error.message);
-    })
+    const redemptionTransaction1 = await redeemChallenge(
+      tokenTypeSponsor,
+      sharedChallenge2.uuid
+    );
+    const provenanceChain2 = await retrieveProvenanceChainFIFO(
+      stampedTokenType.uuid,
+      keypair3.publicKey()
+    );
+  } catch (e) {
+    console.log(e);
   }
 };
 
