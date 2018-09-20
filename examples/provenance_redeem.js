20c5d72750ab81414ce39e8ef238866359ac233f
Dustin Pearson <dustinparkerpearson@gmail.com>
2018-09-20 11:00:14 -0700

    Finish demo testing all of SDK functionality

--- examples/provenance_redeem.js
+++ examples/provenance_redeem.js
@@ -3,7 +3,7 @@ const nCentSDK = require("../source/ncentSDK");
 
 const sdk = new nCentSDK("http://localhost:8010/api");
 
-const TOKEN_NAME = "tttt"; // MUST BE UNIQUE
+const TOKEN_NAME = "aaasa"; // MUST BE UNIQUE
 const TOKEN_COUNT = 10000;
 const EXPIRATION_YEAR = "2021";
 
@@ -38,7 +38,15 @@ const retrieveProvenanceChainFIFO = async (tokenTypeUuid, publicKey) => {
   const retProv = await sdk.retrieveProvenanceChainFIFO(tokenTypeUuid, publicKey);
   const provenanceChain = retProv.data;
   console.log(`Retrieved provenanceChain of oldest owned challenge of ${publicKey} for tokenType ${tokenTypeUuid}\n`);
-  console.log(provenanceChain, '\n');
+  console.log(provenanceChain);
+  return provenanceChain;
+}
+
+const retrieveProvenanceChain = async (transactionUuid) => {
+  const retProv = await sdk.retrieveProvenanceChain(transactionUuid);
+  const provenanceChain = retProv.data;
+  console.log(`Retrieved provenanceChain of transaction ${transactionUuid}:`);
+  console.log(provenanceChain);
   return provenanceChain;
 }
 
@@ -55,6 +63,58 @@ const shareChallenge = async (senderKeypair, receiverPublicKey, transactionUuid)
   return shareChallenge;
 };
 
+const getAllWallets = async () => {
+  const allWalletsRet = await sdk.getWallets();
+  const allWallets = allWalletsRet.data;
+  console.log('Wallets with the following addresses are persisted:');
+  allWallets.forEach((wallet) => {
+    console.log(wallet.address);
+  });
+  return allWallets;
+}
+
+const retrieveBalances = async (walletArray, tokenTypeUuid) => {
+  const balances = [];
+  for (let i = 0; i < walletArray.length; i++) {
+    const walletAddress = walletArray[i].address;
+    const walletBalanceRet = await sdk.getWalletBalance(walletAddress, tokenTypeUuid);
+    const walletBalance = walletBalanceRet.data.balance;
+    balances.push(walletBalance);
+    console.log(`Balance for ${walletAddress}: ${walletBalance}`);
+  }
+  return balances;
+};
+
+const getTokenTypes = async () => {
+  const tokenTypesRet = await sdk.getTokenTypes();
+  const tokenTypes = tokenTypesRet.data;
+  for (let i = 0; i < tokenTypes.length; i++) {
+    console.log(`Token Type #${i}: ${tokenTypes[i].uuid}`);
+  }
+  return tokenTypes;
+}
+
+const getTokenTypeData = async (tokenTypeUuid) => {
+  const tokenTypeDataRet = await sdk.getTokenTypeData(tokenTypeUuid);
+  const tokenTypeData = tokenTypeDataRet.data;
+  const tokenTypeTransactions = tokenTypeData.transactions;
+  console.log(tokenTypeData);
+  console.log(`Data for tokenType ${tokenTypeData.uuid}:`);
+  console.log(`Transactions:`);
+  tokenTypeTransactions.forEach((transaction) => {
+    console.log(transaction);
+  });
+  return tokenTypeData;
+}
+
+const getAllTransactions = async () => {
+  const ret = await sdk.getTransactions();
+  const allTransactions = ret.data;
+  console.log('All Transactions:');
+  console.log(allTransactions);
+  return allTransactions;
+}
+
 const demo = async () => {
   try {
     const tokenTypeSponsor = sdk.createWalletAddress();
@@ -70,6 +130,7 @@ const demo = async () => {
     console.log(
       `${tokenTypeSponsor.publicKey()} created tokenType with UUID ${stampedTokenType.uuid}\n`,
     );
+    const allTokenTypes = await getTokenTypes();
     const sharedChallenge1 = await transferTokens(
       tokenTypeSponsor,
       stampedTokenType.uuid,
@@ -82,11 +143,15 @@ const demo = async () => {
       keypair3,
       100
     );
+    const allWallets1 = await getAllWallets();
+    const allWalletBalances1 = await retrieveBalances(allWallets1, stampedTokenType.uuid);
     const sharedChallenge3 = await shareChallenge(
       keypair2,
       keypair3.publicKey(),
       sharedChallenge1.uuid
     );
+    const allWallets2 = await getAllWallets();
+    const allWalletBalances2 = await retrieveBalances(allWallets2, stampedTokenType.uuid);
     const provenanceChain1 = await retrieveProvenanceChainFIFO(
       stampedTokenType.uuid,
       keypair3.publicKey()
@@ -99,6 +164,11 @@ const demo = async () => {
       stampedTokenType.uuid,
       keypair3.publicKey()
     );
+    const allWallets3 = await getAllWallets();
+    const allWalletBalances3 = await retrieveBalances(allWallets3, stampedTokenType.uuid);
+    const tokenTypeData = await getTokenTypeData(stampedTokenType.uuid);
+    const allTransactions = await getAllTransactions();
+    const provenanceChain3 = await retrieveProvenanceChain(redemptionTransaction1.uuid);
   } catch (e) {
     console.log(e);
   }
